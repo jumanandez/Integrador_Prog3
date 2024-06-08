e0d91371fe3e106464d1d9b10a892fc842cc179d
Ignacio <ignaciomersch602811@gmail.com>
2024-06-08 10:26:30 -0300

    Resolviendo conflicto antes de merge a la rama dev

--- Proyecto.Core.Data/ProjectRepository.cs
+++ Proyecto.Core.Data/ProjectRepository.cs
@@ -18,7 +18,9 @@ namespace Proyecto.Core.Data
 		{
 			_config = config;
 		}
-		public void AddProducto(Producto product)
+
+        #region Region Producto
+        public void AddProducto(Producto product)
 		{
 			
 			using (var dbcontext = new IntegradorProg3Context(_config))
@@ -42,21 +44,21 @@ namespace Proyecto.Core.Data
 				
 			}
 			return productos;
-		}
+        }
 
-		public List<Compra> GetCompras()
-		{
-			var compras = new List<Compra>();
+        public List<Compra> GetCompras()
+        {
+            var compras = new List<Compra>();
 
-			using (var dbcontext = new IntegradorProg3Context(_config))
-			{
-				compras = dbcontext.Compras.ToList();
-			}
-			return compras;
-		}
+            using (var dbcontext = new IntegradorProg3Context(_config))
+            {
+                compras = dbcontext.Compras.ToList();
+            }
+            return compras;
+        }
 
-		public List<Venta> GetVentas() 
-		{
+        public List<Venta> GetVentas()
+        {
             var ventas = new List<Venta>();
 
             using (var dbcontext = new IntegradorProg3Context(_config))
@@ -66,35 +68,35 @@ namespace Proyecto.Core.Data
             return ventas;
         }
 
-		public Producto GetProducto(int id)
-		{
-			var producto = new Producto();
+        public Producto GetProducto(int id)
+        {
+            var producto = new Producto();
 
-			using (var dbcontext = new IntegradorProg3Context(_config))
-			{
-				producto = dbcontext.Productos.Find(id);
-			}
-			return producto;
-		}
+            using (var dbcontext = new IntegradorProg3Context(_config))
+            {
+                producto = dbcontext.Productos.Find(id);
+            }
+            return producto;
+        }
 
-		public void DeleteProducto(int id)
-		{
-			var producto = new Producto();
+        public void DeleteProducto(int id)
+        {
+            var producto = new Producto();
 
-			using(var dbcontext = new IntegradorProg3Context(_config))
-			{
-				producto = dbcontext.Productos.Find(id);
-				dbcontext.Remove(producto);
-				dbcontext.SaveChanges();
-			}
-		}
+            using (var dbcontext = new IntegradorProg3Context(_config))
+            {
+                producto = dbcontext.Productos.Find(id);
+                dbcontext.Remove(producto);
+                dbcontext.SaveChanges();
+            }
+        }
 
-		public int GetStock(int usuarioId, int productoId)
-		{
-			int stock = 0;
-			using (var dbcontext = new IntegradorProg3Context(_config))
-			{
-				var compras = (from c in dbcontext.Compras
+        public int GetStock(int usuarioId, int productoId)
+        {
+            int stock = 0;
+            using (var dbcontext = new IntegradorProg3Context(_config))
+            {
+                var compras = (from c in dbcontext.Compras
                                where c.ProductoId == productoId && c.UsuarioId == usuarioId
                                select c.Cantidad).Sum();
 
@@ -102,9 +104,42 @@ namespace Proyecto.Core.Data
                               where v.ProductoId == productoId && v.UsuarioId == usuarioId
                               select v.Cantidad).Sum();
 
-				stock = compras - ventas;
+                stock = compras - ventas;
             }
             return stock;
         }
-	}
+
+		#endregion
+
+		#region Region Categoria
+		public List<Categoria> GetCategorias()
+		{
+			var categorias = new List<Categoria>();
+
+			using (var dbcontext = new IntegradorProg3Context(_config))
+			{
+
+				categorias = dbcontext.Categoria.ToList();
+			}
+
+			return categorias;
+
+		}
+		public bool AddCategoría(Categoria categoria)
+		{
+			using (var dbcontext = new IntegradorProg3Context(_config))
+			{
+
+				dbcontext.Categoria.Add(categoria);
+
+				dbcontext.SaveChanges();
+
+			}
+
+			return true;
+		}
+		#endregion
+
+    }
 }
+
