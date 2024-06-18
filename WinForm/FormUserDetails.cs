178fda809e8a15cb296b09b7db0a602c8019fdea
fedeth2019@gmail.com <fedeth2019@gmail.com>
2024-06-18 13:14:00 -0300

    #Solucionado error de combo box userdetails

--- WinForm/FormUserDetails.cs
+++ WinForm/FormUserDetails.cs
@@ -79,7 +79,7 @@ namespace WinForm
 
         private void FormUserDetails_Activated(object sender, EventArgs e)
         {
-            cmbUsuarios.DataSource = _usuarioBusiness.GetAllUsuarios().OrderByDescending(u => u.Venta).Count();
+            cmbUsuarios.DataSource = _usuarioBusiness.GetAllUsuarios().OrderByDescending(u => u.Venta.Count()).ToList();
             cmbUsuarios.DisplayMember = "Nombre";
             VentasDataGridView.AutoGenerateColumns = false;
             dataGridViewCompras.AutoGenerateColumns = false;
